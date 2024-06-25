ea3f62e46d69de7b2e4d9985053a8cd4fbb6c8ab
Takegap ( Kleidson Almeida ) <146737840+kleidsonadesign@users.noreply.github.com>
2024-06-25 10:21:52 -0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,12 @@
 
+// utils.js
+function formatPrice(price) {
+  return `$${price.toFixed(2)}`;
+}
+
+function searchProductByName(products, searchTerm) {
+  searchTerm = searchTerm.toLowerCase();
+  return products.filter(product => product.name.toLowerCase().includes(searchTerm));
+}
+
+export { formatPrice, searchProductByName };
